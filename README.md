a1d2b08ab805b66280a2a82fc8aab0af713a9ea8
Arnold SOMOGYI <arnold.somogyi@ext.ec.europa.eu>
2021-03-09 15:45:00 +0100

    initial  commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# FIPS mode checker
+
+_keywords: java, weblogic, fips, FIPS 140-2, RSA JCE, RSA JSSE, JCE, Federal Information Processing Standards_
+
+## 1) Overview
+
+This is a web application that checks whether the FIPS mode is enabled or disabled on the application server.
+More information available about how to enable FIPS mode on Oracle WebLogic server [here](https://docs.oracle.com/middleware/1213/wls/SECMG/fips.htm#SECMG768).
+The web application can be installed to Admin and/or Managed Servers of the application server.
+
+## 2) Licence
+BSD (2-clause) licensed.
---
+++ src/main/java/com/remal/fips/FipsValidator.java
@@ -0,0 +1,109 @@
+package com.remal.fips;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.Provider;
+import java.security.Security;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * FIPS validator servlet.
+ *
+ * <p>Copyright 2021 Arnold Somogyi</p>
+ *
+ * @author arnold.somogyi@gmail.com
+ */
+@WebServlet(value = "/")
+public class FipsValidator extends HttpServlet {
+
+    /**
+     * Called by the server (via the service method) to allow a servlet to handle a GET request.
+     *
+     * @param request object that contains the request the client has made of the servlet
+     * @param response object that contains the response the servlet sends to the client
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        response.setContentType("text/html");
+
+        List<String> providers = getSecurityProviders();
+        try (PrintWriter out = response.getWriter()) {
+            out.print("<html><body>");
+            out.print("<h3>FIPS checker</h3>");
+            out.print(generateProviderReport(providers));
+
+            if (isJsafeJceInstalled(providers) && isRsaJsseInstalled(providers)) {
+                out.print("<p style='color:green; font-weight: bold;'>FIPS 140-2 Mode is enabled</p>");
+            } else {
+                out.print("<p style='color:red; font-weight: bold;'>FIPS 140-2 Mode is disabled</p>");
+            }
+
+            out.print("</body></html>");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Check whether the JSA JCE security provider is loaded or not.
+     *
+     * @param providers the list of the available security providers
+     * @return true if JSA JCE security provider is loaded
+     */
+    private boolean isJsafeJceInstalled(List<String> providers) {
+        for (String provider : providers) {
+            if (provider.toLowerCase().contains("jsafejce")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Check whether the RSA JSSE security provider is loaded or not.
+     *
+     * @param providers the list of the available security providers
+     * @return true if RAS JSSE security provider is loaded
+     */
+    private boolean isRsaJsseInstalled(List<String> providers) {
+        for (String provider : providers) {
+            if (provider.toLowerCase().contains("rsajsse")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Generates a HTML formatted security provider list.
+     *
+     * @param providers the list of the available security providers
+     * @return the HTML formatted security provider list
+     */
+    private String generateProviderReport(List<String> providers) {
+        StringBuilder report = new StringBuilder();
+        report.append("<h4>Supported Java Security Providers:</h4>");
+        report.append("<ul>");
+        for (String provider : providers) {
+            report.append("<li>").append(provider).append("</li>");
+        }
+        report.append("</ul>");
+
+        return report.toString();
+    }
+
+    private List<String> getSecurityProviders() {
+        List<String> providerNames = new ArrayList<>();
+        Provider[] providers = Security.getProviders();
+        for (Provider provider : providers) {
+            providerNames.add(provider.getName());
+        }
+
+        return providerNames;
+    }
+}
